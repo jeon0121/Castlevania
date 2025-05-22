bbd8541a593c22328dfb70f7b513188f1d6d573c
jeon0121 <jamie0121304@gmail.com>
2025-05-23 06:11:46 +0800

    create stage3

---
+++ src/State/Stage3.cpp
@@ -0,0 +1,102 @@
+#include "State/Stage3.hpp"
+#include "Utility/Position.hpp"
+
+void Stage3::Start(App* app){
+    // menu
+    app->m_Menu->SetMenuVisibility(true);
+
+    // background
+    m_Background = std::make_shared<ImageItems>(GA_RESOURCE_DIR"/background/stage-3/phantom-bat.png");
+    m_Background->m_Transform.scale = glm::vec2(1.025, 0.90);
+    m_Background->SetPosition({1050, -35});
+    backgroundPos = m_Background->GetPosition();
+    m_All.push_back(m_Background);
+
+    // blink
+    // std::vector<std::string> blinkImages = {GA_RESOURCE_DIR"/background/stage-1/entrance-hall-blink.png", GA_RESOURCE_DIR"/background/stage-1/entrance-hall.png"};
+    // m_Blink = std::make_shared<AnimatedItems>(blinkImages, 40, m_Background->m_Transform.scale);
+    // m_Blink->SetZIndex(1);
+    // m_Blink->SetPosition(m_Background->GetPosition());
+    // m_Blink->SetVisible(false);
+    // m_All.push_back(m_Blink);
+
+    //character
+    if (!app->m_Character) {
+        CharacterValue charactervalue;
+        charactervalue.position = glm::vec2(-315, 80.76);
+        charactervalue.direction = "right";
+        charactervalue.beIndex = 2;
+        app->m_Character = std::make_shared<Character>(charactervalue);
+    }
+    else
+        app->m_Character->SetPosition({-315, 80.76});
+    this->m_Character = app->m_Character;
+    m_All.push_back(m_Character->m_Behavior);
+
+    //torch
+    std::vector<TorchData> torchs = {
+        { { -393, -125 }, {1, 0.9}, LootType::HeartBig,   1},
+        { { -131,  106 }, {1, 0.9}, LootType::HeartSmall, 1},
+        { { 131,   -10 }, {1, 0.9}, LootType::RedBag,     1},
+        { { 394,   106 }, {1, 0.9}, LootType::Pot,        1},
+        { { 656,  -240 }, {1, 0.9}, LootType::HeartSmall, 1},
+        { { 914,  -125 }, {1, 0.9}, LootType::Axe,        1},
+        { { 1177, -240 }, {1, 0.9}, LootType::HeartSmall, 1},
+        { { 1440, -240 }, {1, 0.9}, LootType::HeartSmall, 1},
+        { { 1702, -125 }, {1, 0.9}, LootType::HeartBig,   1},
+        { { 1964, -240 }, {1, 0.9}, LootType::HeartSmall, 1},
+        { { 2227, -125 }, {1, 0.9}, LootType::HeartSmall, 1},
+        { { 2489, -125 }, {1, 0.9}, LootType::HeartBig,   1}
+    };
+    for (auto& t : torchs) {
+        auto torch = std::make_shared<Torch>(t.pos, t.scale, t.loot, t.type);
+        m_Torches.push_back(torch);
+        m_All.push_back(torch);
+    }
+
+    // block
+    std::vector<BlockData> blocks = {
+         { { 0,    -352 }, { 100,  0.7  } },
+         { { -547, -50  }, { 0.4,  7    } },
+         { { -263, -5   }, { 4.26, 0.68 } },
+         { { 393,  -122 }, { 6.42, 0.68 } },
+
+         { { 2295, -240 }, { 1.08, 0.68 } },
+         { { 2500, -122 }, { 2.2,  0.68 } },
+
+         { { 2610, -240 }, { 0.3,  2    } },
+         { { 2610,  160 }, { 0.3,  2    } },
+    };
+    for (auto& b : blocks) {
+        auto block = std::make_shared<Block>(b.pos, b.scale);
+        m_Blocks.push_back(block);
+        m_All.push_back(block);
+    }
+
+    // stair
+    std::vector<StairData> stairs = {
+        { { -70,   48  }, { 68,   -66 } },
+        { { 716,  -66  }, { 985, -294 } },
+        { { 2295, -180 }, { 2430, -66 } }
+    };
+    for (auto& s : stairs) {
+        auto stair = Stair::CreateStair(s.pos1, s.pos2);
+        m_Stairs.insert(m_Stairs.end(), stair.begin(), stair.end());
+        m_All.insert(m_All.end(), stair.begin(), stair.end());
+    }
+
+    app->AddAllChildren(m_All);
+    m_stateState = StateState::UPDATE;
+}
+
+void Stage3::Update(App* app){
+    // Position::PrintObjectCoordinate(m_Character, offsetX);
+    m_Character->Keys(m_Blocks, m_Stairs);
+    UpdateTorch(app);
+    UpdateSubWeapon(app);
+    UpdateScroll(mapWidth);
+}
+
+void Stage3::End(App* app){
+
+}
