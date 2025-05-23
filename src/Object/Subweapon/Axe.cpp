bd61be8aa57c71475a07129c997644527125c744
ilovemath25 <w3573y.young@gmail.com>
2025-05-23 17:21:57 +0800

    add axe

--- src/Object/Subweapon/Axe.cpp
+++ src/Object/Subweapon/Axe.cpp
@@ -1,7 +1,11 @@
 #include "Object/Subweapon/Axe.hpp"
 
 namespace Subweapon {
-    Axe::Axe(glm::vec2 position, std::string type) : Loot(position, {GA_RESOURCE_DIR "/items/weapon/axe.png"}, 0, LootType::Axe), m_type(type) {}
+    Axe::Axe(glm::vec2 position, std::string type) : Loot(position, {GA_RESOURCE_DIR "/items/weapon/axe.png"}, 0, LootType::Axe), m_type(type) {
+        if (m_type == "subweapon") {
+            y_vel = 15.0f;
+        }
+    }
 
     void Axe::Result(App* app, std::shared_ptr<Character> &character, std::shared_ptr<Menu> &menu) {
         menu->modifyWeapon(WeaponType::Axe);
@@ -11,6 +15,21 @@ namespace Subweapon {
     }
 
     void Axe::Use(const std::vector<std::shared_ptr<Block>>& m_Blocks) {
-
+        (void) m_Blocks;
+        glm::vec2 pos = UpdatePosition();
+        SetPosition(pos + glm::vec2(x_vel, y_vel));
+        x_vel = m_direction == "left" ? -8
+              : m_direction == "right" ? 8 : 0;
+        static int delayCount = 0;
+        delayCount++;
+        if (delayCount >= 5) {
+            m_Transform.rotation += 90;
+            delayCount = 0;
+            if (m_Transform.rotation >= 360)
+                m_Transform.rotation = 0;
+        }
+        y_vel = std::max(y_vel - ((-2.0f <= y_vel && y_vel <= 2.0f) ? 0.3f : 1.0f), -15.0f);
+        if (pos.y < -400.0f) 
+            if_destroyed = true;
     }
 }
